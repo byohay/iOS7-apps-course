63eb0efd4512b412c96d5735f0b0fe45d7c7656c
Ben Yohay <beny@Bens-iMac.local>
2016-03-28 18:58:31 +0300

    removed every relation to flip counter

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -12,8 +12,6 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UILabel *flips_label;
-@property (nonatomic) int flipsCount;
 @property (strong, nonatomic) Deck* deck;
 
 @end
@@ -21,43 +19,12 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)setFlipsCount:(int)flipsCount
 {
-    _flipsCount = flipsCount;
-    self.flips_label.text = [NSString stringWithFormat:@"Flips: %d", self.flipsCount];
 }
 
 
 - (IBAction)touchCardButton:(UIButton *)sender {
-    if ([sender.currentTitle length]){
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
-                          forState: (UIControlStateNormal)];
-        [sender setTitle:@"" forState: (UIControlStateNormal)];
-    }
-    else{
-        Card* card = [self.deck drawRandomCard];
-        
-        if (!card) {
-            return;
-        }
-
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState: (UIControlStateNormal)];
-        
-        [sender setTitle:card.contents forState: (UIControlStateNormal)];
-    }
-    
-    self.flipsCount++;
 }
 
 - (Deck*)deck
